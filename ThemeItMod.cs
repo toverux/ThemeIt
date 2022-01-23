cca52bf594d30d928a4f97f5ebd68fc18aa3c943
Morgan Touverey Quilling <morgan.touverey@outlook.fr>
2022-01-23 22:21:04 +0100

    chore: base mod skeleton

---
+++ ThemeItMod.cs
@@ -0,0 +1,9 @@
+﻿using ICities;
+
+namespace ThemeIt {
+    public class ThemeItMod : IUserMod {
+        public string Name => "Theme It";
+
+        public string Description => "Create themes for growables and apply them to cities and districts.";
+    }
+}
