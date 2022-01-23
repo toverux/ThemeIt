446ae904882e398114d71caa54e6e5dd44e71431
Morgan Touverey Quilling <morgan.touverey@outlook.fr>
2022-01-23 23:44:51 +0100

    feat: demo harmony patcher

---
+++ HarmonyPatcher.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ThemeIt {
+    public static class HarmonyPatcher {
+        private const string HarmonyId = "toverux/ThemeIt";
+
+        private static bool patched;
+
+        public static void PatchAll() {
+            if (HarmonyPatcher.patched) {
+                return;
+            }
+
+
+            HarmonyPatcher.patched = true;
+
+            var harmony = new Harmony(HarmonyPatcher.HarmonyId);
+
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            
+            Debug.Log("ThemeIt: Game routines have been patched.");
+        }
+
+        public static void UnpatchAll() {
+            if (!HarmonyPatcher.patched) {
+                return;
+            }
+
+            var harmony = new Harmony(HarmonyPatcher.HarmonyId);
+
+            harmony.UnpatchAll(HarmonyPatcher.HarmonyId);
+
+            HarmonyPatcher.patched = false;
+
+            Debug.Log("ThemeIt: Game routines have been restored.");
+        }
+    }
+
+    [HarmonyPatch(typeof(SimulationManager), "CreateRelay")]
+    public static class SimulationManagerCreateRelayPatch {
+        public static void Prefix() {
+            Debug.Log("ThemeIt: CreateRelay Prefix");
+        }
+    }
+
+    [HarmonyPatch(typeof(LoadingManager), "MetaDataLoaded")]
+    public static class LoadingManagerMetaDataLoadedPatch {
+        public static void Prefix() {
+            Debug.Log("ThemeIt: MetaDataLoaded Prefix");
+        }
+    }
+}
--- ThemeItMod.cs
+++ ThemeItMod.cs
@@ -1,9 +1,20 @@
-﻿using ICities;
+﻿using CitiesHarmony.API;
+using ICities;
 
 namespace ThemeIt {
     public class ThemeItMod : IUserMod {
         public string Name => "Theme It";
 
         public string Description => "Create themes for growables and apply them to cities and districts.";
+
+        public void OnEnabled() {
+            HarmonyHelper.DoOnHarmonyReady(HarmonyPatcher.PatchAll);
+        }
+
+        public void OnDisabled() {
+            if (HarmonyHelper.IsHarmonyInstalled) {
+                HarmonyPatcher.UnpatchAll();
+            }
+        }
     }
 }
