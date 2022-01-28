23184a38cada4956f8201492c2e50ef2d937176a
Morgan Touverey Quilling <morgan.touverey@outlook.fr>
2022-01-28 20:51:43 +0100

    feat: replace calls to GetRandomBuildingInfo with patch at call sites

---
+++ ThemeIt/Patches.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using ColossalFramework.Math;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ThemeIt;
+
+public static class Patches {
+    private static readonly MethodInfo GetRandomBuildingInfoMethod = AccessTools.Method(
+        typeof(BuildingManager),
+        nameof(BuildingManager.GetRandomBuildingInfo));
+
+    private static readonly MethodInfo GetRandomBuildingInfoPatchMethod = AccessTools.Method(
+        typeof(Patches),
+        nameof(Patches.GetRandomBuildingInfoPatch));
+    
+    public static IEnumerable<CodeInstruction> TranspileZoneBlockSimulationStep(
+        IEnumerable<CodeInstruction> instructions) =>
+        Patches.TranspileGetRandomBuildingInfoConsumer(instructions, 64);
+    
+    public static IEnumerable<CodeInstruction> TranspilePrivateBuildingAiGetUpgradeInfo(
+        IEnumerable<CodeInstruction> instructions) =>
+        Patches.TranspileGetRandomBuildingInfoConsumer(instructions, 4);
+
+    public static IEnumerable<CodeInstruction> TranspilePrivateBuildingAiSimulationStep(
+        IEnumerable<CodeInstruction> instructions) =>
+        Patches.TranspileGetRandomBuildingInfoConsumer(instructions, 21);
+
+    public static IEnumerable<CodeInstruction> TranspileGetRandomBuildingInfoConsumer(
+        IEnumerable<CodeInstruction> instructions,
+        int districtLocalVariableIndex) {
+
+        var targetSiteFound = false;
+
+        foreach (var instruction in instructions) {
+            if (instruction.Is(OpCodes.Callvirt, Patches.GetRandomBuildingInfoMethod)) {
+                targetSiteFound = true;
+
+                yield return new CodeInstruction(OpCodes.Ldloc_S, districtLocalVariableIndex);
+                yield return new CodeInstruction(OpCodes.Call, Patches.GetRandomBuildingInfoPatchMethod);
+            }
+            else {
+                yield return instruction;
+            }
+        }
+
+        if (!targetSiteFound) {
+            throw new Exception("Cannot find target site to apply patch on.");
+        }
+    }
+
+    private static BuildingInfo GetRandomBuildingInfoPatch(
+        BuildingManager instance,
+        ref Randomizer randomizer,
+        ItemClass.Service service,
+        ItemClass.SubService subService,
+        ItemClass.Level level,
+        int width,
+        int length,
+        BuildingInfo.ZoningMode zoningMode,
+        int style,
+        byte district) {
+
+        Debug.Log("Called GetRandomBuildingInfo");
+        Debug.Log($"service={service} subService={subService} level={level} width={width} length={length} zoningMode={zoningMode} district={district}");
+
+        return instance.GetRandomBuildingInfo(
+            ref randomizer, service, subService, level, width, length, zoningMode, style);
+    }
+}
--- ThemeIt/ThemeItMod.cs
+++ ThemeIt/ThemeItMod.cs
@@ -1,20 +1,45 @@
-﻿using CitiesHarmony.API;
-using ICities;
+﻿using System;
+using System.Collections.Generic;
+using ModsCommon;
 
 namespace ThemeIt {
-    public class ThemeItMod : IUserMod {
-        public string Name => "Theme It";
+    // ReSharper disable once UnusedType.Global
+    public class ThemeItMod : BasePatcherMod<ThemeItMod> {
+        public override string NameRaw => "Theme It";
 
-        public string Description => "Create themes for growables and apply them to cities and districts.";
+        public override List<Version> Versions => new() {
+            new Version(1,0,0)
+        };
 
-        public void OnEnabled() {
-            HarmonyHelper.DoOnHarmonyReady(HarmonyPatcher.PatchAll);
-        }
+        protected override string IdRaw => "ThemeIt";
 
-        public void OnDisabled() {
-            if (HarmonyHelper.IsHarmonyInstalled) {
-                HarmonyPatcher.UnpatchAll();
-            }
+        public override bool IsBeta => false;
+
+        public override string Description => "Create themes for growables and apply them to cities and districts.";
+
+        protected override ulong StableWorkshopId => 0;
+
+        protected override ulong BetaWorkshopId => 0;
+
+        protected override bool PatchProcess() {
+            var success = true;
+
+            success &= this.AddTranspiler(
+                typeof(Patches), nameof(Patches.TranspileZoneBlockSimulationStep),
+                typeof(ZoneBlock), nameof(ZoneBlock.SimulationStep),
+                new[] { typeof(ushort) });
+
+            success &= this.AddTranspiler(
+                typeof(Patches), nameof(Patches.TranspilePrivateBuildingAiGetUpgradeInfo),
+                typeof(PrivateBuildingAI), nameof(PrivateBuildingAI.GetUpgradeInfo),
+                new[] { typeof(ushort), typeof(Building).MakeByRefType() });
+
+            success &= this.AddTranspiler(
+                typeof(Patches), nameof(Patches.TranspilePrivateBuildingAiSimulationStep),
+                typeof(PrivateBuildingAI), nameof(PrivateBuildingAI.SimulationStep),
+                new[] { typeof(ushort), typeof(Building).MakeByRefType(), typeof(Building.Frame).MakeByRefType() });
+
+            return success;
         }
     }
 }
